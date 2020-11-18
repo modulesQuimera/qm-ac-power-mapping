9961fe2a48963de58d76552e576646dd2c18bc4a
SCROLLUCK <llc2@icomp.ufam.edu.br>
2020-11-18 12:09:26 -0400

    Minor fix validate global map

--- mapping-voltage.js
+++ mapping-voltage.js
@@ -1,6 +1,6 @@
 module.exports = function(RED) {
 
-    function mappingSetVoltagetNode(config) {
+    function mappingSetVoltageNode(config) {
         RED.nodes.createNode(this, config);
         this.name = config.name;
         this.slot = config.slot;
@@ -23,24 +23,27 @@ module.exports = function(RED) {
         var globalContext = this.context().global;
         var map = globalContext.get("map");
 
-        map.ac_power[this.slot - 1][0].pin =  "AC-POWER Slot " + String(this.slot);
-        map.ac_power[this.slot - 1][0].board =  "";
-        map.ac_power[this.slot - 1][0].user = "";
+        if(map) {
+            map.ac_power[this.slot - 1][0].pin =  "AC-POWER Slot " + String(this.slot);
+            map.ac_power[this.slot - 1][0].board =  "";
+            map.ac_power[this.slot - 1][0].user = "";
+    
+            map.ac_power[this.slot - 1][1].pin =  "VA - PIN - 12";
+            map.ac_power[this.slot - 1][1].board =  "TP" + String(this.labelPort1);
+            map.ac_power[this.slot - 1][1].user = this.labelPort1A;
+    
+            map.ac_power[this.slot - 1][2].pin =  "VB - PIN - 13";
+            map.ac_power[this.slot - 1][2].board =  "TP" + String(this.labelPort2);
+            map.ac_power[this.slot - 1][2].user = this.labelPort2A;
+    
+            map.ac_power[this.slot - 1][3].pin =  "VC - PIN - 14";
+            map.ac_power[this.slot - 1][3].board =  "TP" + String(this.labelPort3);
+            map.ac_power[this.slot - 1][3].user = this.labelPort3A;   
+        }
 
-        map.ac_power[this.slot - 1][1].pin =  "VA - PIN - X";
-        map.ac_power[this.slot - 1][1].board =  "TP" + String(this.labelPort1);
-        map.ac_power[this.slot - 1][1].user = this.labelPort1A;
-
-        map.ac_power[this.slot - 1][2].pin =  "VA - PIN - X";
-        map.ac_power[this.slot - 1][2].board =  "TP" + String(this.labelPort2);
-        map.ac_power[this.slot - 1][2].user = this.labelPort2A;
-
-        map.ac_power[this.slot - 1][3].pin =  "VA - PIN - X";
-        map.ac_power[this.slot - 1][3].board =  "TP" + String(this.labelPort3);
-        map.ac_power[this.slot - 1][3].user = this.labelPort3A;
 
         globalContext.set("map", map);
     }
 
-    RED.nodes.registerType("mapping-voltage", mappingSetVoltagetNode);
+    RED.nodes.registerType("mapping-voltage", mappingSetVoltageNode);
 };
